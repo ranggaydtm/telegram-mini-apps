57b10d41fe0dcf927694873f2ccb44afaf12305f
ranggaydtm <ranggay67@gmail.com>
2024-12-02 13:14:46 +0700

    fix firebase config

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -2,30 +2,28 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, Firestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDv6elUl1Q1S9QD_Iw53nKTYqBc9Yiu8GI",
-  authDomain: "telegram-task-af808.firebaseapp.com",
-  projectId: "telegram-task-af808",
-  storageBucket: "telegram-task-af808.firebasestorage.app",
-  messagingSenderId: "295344938884",
-  appId: "1:295344938884:web:9b8ab2f0d8d1ba983fc7d1",
-  measurementId: "G-CL83Z6ZWZR"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 let app;
-export let db: Firestore;
+let db: Firestore;
 
 if (typeof window !== 'undefined') {
     app = initializeApp(firebaseConfig)
     db = getFirestore(app)
 }
 
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+export { db };
